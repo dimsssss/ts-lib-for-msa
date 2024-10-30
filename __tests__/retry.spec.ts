91b7aa411d291ff6733dd58c7989034daffbdce2
dev <caruen@hanmail.net>
2024-10-31 01:22:07 +0900

    refactor: count -> callbackExecuteCount

--- __tests__/retry.spec.ts
+++ __tests__/retry.spec.ts
@@ -2,12 +2,12 @@ import { retry } from "../lib/retry";
 
 describe('Retry', () => {
     test('콜백(첫번째 에러를 발생시키고 두번째 success를 반환함)을 받고 retry를 하면 success를 반환해야한다.', () => {
-        let count = 0
+        let callbackExecuteCount = 0
         const retryCount = 2
 
         expect(retry(() => {
-            if (count == 0) {
-                count += 1
+            if (callbackExecuteCount == 0) {
+                callbackExecuteCount += 1
                 throw new Error("An error occurred");
             }
             return "success"
@@ -15,12 +15,12 @@ describe('Retry', () => {
     })
 
     test('콜백(두번의 에러를 발생시키고 마지막으로 success를 반환함)을 받고 retry를 하면 success를 반환해야한다.', () => {
-        let count = 0
+        let callbackExecuteCount = 0
         const retryCount = 3
 
         expect(retry(() => {
-            if (count < 2) {
-                count += 1
+            if (callbackExecuteCount < 2) {
+                callbackExecuteCount += 1
                 throw new Error("An error occurred");
             }
             return "success"
@@ -28,13 +28,13 @@ describe('Retry', () => {
     })
 
     test('에러가 없으면 retry하지 않는다', () => {
-        let count = 0
+        let callbackExecuteCount = 0
         const retryCount = 3
 
         expect(retry(() => {
-            count += 1
+            callbackExecuteCount += 1
             return "success"
         }, retryCount)).toEqual("success");
-        expect(count).toEqual(1)
+        expect(callbackExecuteCount).toEqual(1)
     })
 })
